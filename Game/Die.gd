4386df1c63406d7b41b400d273098c7ccb84d1cc
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2022-07-16 21:42:09 -0400

    fix signal bug

--- Game/Die.gd
+++ Game/Die.gd
@@ -14,12 +14,13 @@ func _ready():
 
 func roll(enemy):
 	get_parent().selected_die = null
-	connect("player_rolled", enemy, "_damage_calc")	
+	connect("player_rolled", enemy, "_damage_calc")
 	get_parent().state = PlayerState.ENEMY_TURN
 	rng.randomize()
 	var result = sides[rng.randi_range(0, sides.size()-1)]
 	Events.emit_signal("text_log_push", "You rolled a {result}.".format({"result":result}))
 	emit_signal("player_rolled", result)
+	disconnect("player_rolled", enemy, "_damage_calc")
 	times_rolled+=1
 
 func _on_Button_pressed():
